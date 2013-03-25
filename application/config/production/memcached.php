995c2a5a11accc95adf5055e6eb7f07cdb1dff85
xiefie <xiefie.admin@gmail.com>
2013-03-25 22:04:40 +0800

    memcached

---
+++ application/config/production/memcached.php
@@ -0,0 +1,11 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+$config['memcached1'] = array(
+		'hostname' => '127.0.0.1',
+		'port'     => 11211,
+		'weight'   => 1,
+		);
+$config['memcached2'] = array(
+		'hostname' => '127.0.0.1',
+		'port'     => 11212,
+		'weight'   => 1,
+		);
