42d5e3b5a0ad904265a15160920ca67eb5608f46
linchg <linchg@gmail.com>
2013-03-26 22:00:25 +0800

    update

--- application/libraries/Login.php
+++ application/libraries/Login.php
@@ -5,16 +5,29 @@ class Login
     public function __construct() {
         $this->CI =& get_instance();
     }	
+
+    public function checkLogin($username, $password){
+        $user = $this->get($username, "username");
+        if(empty($user)){
+            return false;
+        }
+        if($user['password'] === md5(config_item('user_pass_prefix').$password)){
+            return $user;
+        }else{
+            return false;
+        }
+    }
 	
 	public function is_login()
 	{
-		return true;
+        return $this->CI->session->userdata('id') ? true : false;
 	} 
 
 	public function get_user()
 	{
 		return array('username' => 'test');
 	}
+
 	public function login_out()
 	{
 		return true;
