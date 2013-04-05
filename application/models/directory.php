ddb5542fd1dacfe160c793ef18e31c9201986ad7
linchg <linchg@gmail.com>
2013-04-05 22:44:31 +0800

    update

--- application/models/directory.php
+++ application/models/directory.php
@@ -11,7 +11,18 @@ class Directory extends CI_Model {
         $this->_table_name = 'directory';
     }
 
-    function getCount($uid,$where=array()){
+    //获取一个目录的所有子目录
+    public function get_children_dirs($uid,$id){
+        $id = intval($id);
+        if($id <= 0)
+            return array(); 
+        $where = array('fid'=>$id,'uid'=>$uid);
+        $query = $this->db->get_where($this->_table_name,$where);
+        return $query->result_array();
+    }
+
+    //获得一个用户所创建的所有目录
+   public function getCount($uid,$where=array()){
         $uid = (int)$uid;
         if($uid <= 0)
             return false;
@@ -23,7 +34,8 @@ class Directory extends CI_Model {
         return $this->db->count_all_results();
     }
 
-    function get($uid,$where=array(),$order_by=''){
+    //获取一个用户目录的信息
+    public function get($uid,$where=array(),$order_by=''){
         $uid = (int)$uid;
         if($uid <= 0)
             return array(); 
@@ -36,15 +48,43 @@ class Directory extends CI_Model {
         return $query->result_array();
     }
 
-    function create($values){
+    //获取一个目录的具体信息
+    public function get_directory($id){
+        $id = (int)$id;
+        if($id <= 0)
+            return array(); 
+        $where = array('id' => $id);
+        $query = $this->db->get_where($this->_table_name, $where);
+        return $query->row_array();
+    }
 
-        if (!isset($values['uid']) || !isset($values['did']))
+    //判断当前目录下还能否建立子目录
+    //$id 当前目录id
+    //$max_depth 运行的最多层数
+    public function can_create($id,$max_depth){
+        for($i = 1; $i < $max_depth; $i++)
+        {
+            $row = $this->get_directory($id);
+            if(empty($row))
+                return false;
+            if($row['fid'] == 0) 
+                return true;
+            $id = $row['fid'];
+        }
+        return false; 
+    }
+
+    //创建一个目录
+    public function create($values){
+
+        if (!isset($values['uid']) || !isset($values['fid']))
             return false;
         $this->db->insert($this->_table_name, $values);
         return $this->db->insert_id();
     }
 
-    function update($value_array, $where){
+    //修改一个目录
+    public function update($value_array, $where){
 
         if (!isset($where['uid']) && !isset($where['id']))
             return false;
@@ -55,15 +95,24 @@ class Directory extends CI_Model {
         return $this->db->update($this->_table_name, $value_array);
     }
 
-    function delete($where){
+    //删除一个目录
+    public function delete($where){
         
-        if (!isset($where['uid']) && !isset($where['id']))
-            return false;
-        if(isset($where['uid']) && !isset($where['did']))
+        if (!isset($where['id']) )
             return false;
 
         $this->db->where($where);
         return $this->db->delete($this->_table_name);
+    }
+
+    function set($up_array, $where){
+        if (!isset($where['id']))
+            return false;
+
+        $this->db->where($where);
+        foreach($up_array as $k => $v)
+            $this->db->set($k,$v,false);
+        return $this->db->update($this->_table_name);
     }
 }
 
